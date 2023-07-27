b05d3526ae03c67308c2c7f601592e52abe75348
dangthanh03 <111256181+dangthanh03@users.noreply.github.com>
2023-07-27 21:45:27 +0700

    Add files via upload

---
+++ Migrations/20230716143236_IdAddressForUserFix.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RunGroopWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdAddressForUserFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+           name: "AddressId",
+           table: "AspNetUsers",
+           type: "int",
+           nullable: true,
+           defaultValue: 0,
+           oldClrType: typeof(int),
+           oldType: "int",
+           oldNullable: true);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+             name: "AddressId",
+             table: "AspNetUsers",
+             type: "int",
+             nullable: false,
+             defaultValue: 0,
+             oldClrType: typeof(int),
+             oldType: "int",
+             oldNullable: true);
+        }
+    }
+}
---
+++ Migrations/20230716145444_userIdAddress.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RunGroopWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class userIdAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+              name: "FK_AspNetUsers_Addresses_AddressId",
+              table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+           name: "AddressId",
+           table: "AspNetUsers",
+           type: "int",
+           nullable: true,
+           defaultValue: 0,
+           oldClrType: typeof(int),
+           oldType: "int",
+           oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
